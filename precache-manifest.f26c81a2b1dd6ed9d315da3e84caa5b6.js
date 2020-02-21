a79ce337aca4d6eaf3cdaa765d3388c18f99194d
Annie Cypcar <annecypcar@gmail.com>
2020-02-21 07:31:23 -0500

    initial commit

---
+++ precache-manifest.f26c81a2b1dd6ed9d315da3e84caa5b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcef2b6dbcfbcbc36cf376e5e8578137",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dd6391c51ff5350df49",
+    "url": "/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "0ef9163220538c896a68",
+    "url": "/static/js/2.3f420818.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3f420818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd6391c51ff5350df49",
+    "url": "/static/js/main.0807e89c.chunk.js"
+  },
+  {
+    "revision": "3d7c5317cc76ac9b9de3",
+    "url": "/static/js/runtime-main.bbeaef75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
